9fed5365705c4362094584feb5374b80960c4d45
Matteo Matassoni <4108197+matax87@users.noreply.github.com>
2023-10-17 12:12:45 +0200

    Initial Commit

---
+++ DMNMulticastDelegate/DMNMulticastDelegate.h
@@ -0,0 +1,18 @@
+//
+//  DMNMulticastDelegate.h
+//  DMNMulticastDelegate
+//
+//  Created by Matteo Matassoni on 17/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DMNMulticastDelegate.
+FOUNDATION_EXPORT double DMNMulticastDelegateVersionNumber;
+
+//! Project version string for DMNMulticastDelegate.
+FOUNDATION_EXPORT const unsigned char DMNMulticastDelegateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMNMulticastDelegate/PublicHeader.h>
+
+
